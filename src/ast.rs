9a62cceeb38fa910a14cc52649740bce50294204
36H <admin@36h.top>
2024-04-16 22:19:03 +0800

    lv3的IR生成

--- src/ast.rs
+++ src/ast.rs
@@ -28,8 +28,30 @@ pub struct Return {
 }
 #[derive(Debug)]
 pub struct Exp {
-    pub unary_exp: UnaryExp,
+    pub lor: LOrExp,
 }
+#[derive(Debug)]
+pub enum LAndExp {
+    Eq(EqExp),
+    LAndEq(Box<LAndExp>, EqExp),
+}
+#[derive(Debug)]
+pub enum RelExp {
+    Add(AddExp),
+    RelAdd(Box<RelExp>, RelOp, AddExp),
+}
+#[derive(Debug)]
+pub enum EqExp {
+    Rel(RelExp),
+    EqRel(Box<EqExp>, EqOp, RelExp),
+}
+
+#[derive(Debug)]
+pub enum LOrExp {
+    LAnd(LAndExp),
+    LOrLAnd(Box<LOrExp>, LAndExp),
+}
+
 #[derive(Debug)]
 pub enum UnaryExp {
     Primary(PrimaryExp),
@@ -44,4 +66,49 @@ pub enum PrimaryExp {
 pub enum UnaryOp {
     Neg,
     LNot,
+}
+
+#[derive(Debug)]
+pub enum MulExp {
+    Unary(UnaryExp),
+    MulUnary(Box<MulExp>, MulOp, UnaryExp),
+}
+
+#[derive(Debug)]
+pub enum AddExp {
+    Mul(MulExp),
+    AddMul(Box<AddExp>, AddOp, MulExp),
+}
+
+#[derive(Debug)]
+pub enum MulOp {
+    // *
+    Mul,
+    // "/"
+    Div,
+    // "%"
+    Mod,
+}
+
+#[derive(Debug)]
+pub enum AddOp {
+    // "+"
+    Add,
+    // "-"
+    Sub,
+}
+
+#[derive(Debug)]
+pub enum RelOp {
+    Lt,
+    Gt,
+    Le,
+    Ge,
+}
+#[derive(Debug)]
+pub enum EqOp {
+    // "=="
+    Eq,
+    // "!="
+    Neq,
 }
--- src/irgen/generate.rs
+++ src/irgen/generate.rs
@@ -127,41 +127,233 @@ impl<'ast> GenerateProgram<'ast> for Exp {
     type Out = ExpValue;
 
     fn generate(&'ast self, program: &mut Program, scopes: &mut Scopes<'ast>) -> Result<Self::Out> {
-        self.unary_exp.generate(program, scopes)
+        self.lor.generate(program, scopes)
     }
 }
 
 impl<'ast> GenerateProgram<'ast> for PrimaryExp {
     type Out = ExpValue;
-  
-    fn generate(&'ast self, program: &mut Program, scopes: &mut Scopes<'ast>) -> Result<Self::Out> {
-      match self {
-        Self::Exp(exp) => exp.generate(program, scopes),
-        Self::Number(num) => Ok(ExpValue::Int(
-          cur_func!(scopes).new_value(program).integer(*num),
-        )),
-      }
-    }
-  }
 
+    fn generate(&'ast self, program: &mut Program, scopes: &mut Scopes<'ast>) -> Result<Self::Out> {
+        match self {
+            Self::Exp(exp) => exp.generate(program, scopes),
+            Self::Number(num) => Ok(ExpValue::Int(
+                cur_func!(scopes).new_value(program).integer(*num),
+            )),
+        }
+    }
+}
 
 impl<'ast> GenerateProgram<'ast> for UnaryExp {
     type Out = ExpValue;
-  
+
     fn generate(&'ast self, program: &mut Program, scopes: &mut Scopes<'ast>) -> Result<Self::Out> {
-      match self {
-        Self::Primary(exp) => exp.generate(program, scopes),
-        Self::Unary(op, exp) => {
-          let exp = exp.generate(program, scopes)?.into_int(program, scopes)?;
-          let info = cur_func!(scopes);
-          let zero = info.new_value(program).integer(0);
-          let value = match op {
-            UnaryOp::Neg => info.new_value(program).binary(BinaryOp::Sub, zero, exp),
-            UnaryOp::LNot => info.new_value(program).binary(BinaryOp::Eq, exp, zero),
-          };
-          info.push_inst(program, value);
-          Ok(ExpValue::Int(value))
+        match self {
+            Self::Primary(exp) => exp.generate(program, scopes),
+            Self::Unary(op, exp) => {
+                let exp = exp.generate(program, scopes)?.into_int(program, scopes)?;
+                let info = cur_func!(scopes);
+                let zero = info.new_value(program).integer(0);
+                let value = match op {
+                    UnaryOp::Neg => info.new_value(program).binary(BinaryOp::Sub, zero, exp),
+                    UnaryOp::LNot => info.new_value(program).binary(BinaryOp::Eq, exp, zero),
+                };
+                info.push_inst(program, value);
+                Ok(ExpValue::Int(value))
+            }
         }
-      }
     }
-  }
+}
+
+/// Generates logical operators.
+macro_rules! generate_logical_ops {
+    (
+      $lhs:expr, $rhs:expr, $program:expr, $scopes:expr,
+      $prefix:literal, $rhs_bb:ident, $end_bb:ident, $tbb:ident, $fbb:ident
+    ) => {{
+        // generate result
+        let result = cur_func!($scopes).new_alloc($program, Type::get_i32(), None);
+        // generate left-hand side expression
+        let lhs = $lhs
+            .generate($program, $scopes)?
+            .into_int($program, $scopes)?;
+        let info = cur_func_mut!($scopes);
+        let zero = info.new_value($program).integer(0);
+        let lhs = info.new_value($program).binary(BinaryOp::NotEq, lhs, zero);
+        info.push_inst($program, lhs);
+        let store = info.new_value($program).store(lhs, result);
+        info.push_inst($program, store);
+        // generate basic blocks and branch
+        let $rhs_bb = info.new_bb($program, Some(concat!("%", $prefix, "_rhs")));
+        let $end_bb = info.new_bb($program, Some(concat!("%", $prefix, "_end")));
+        let br = info.new_value($program).branch(lhs, $tbb, $fbb);
+        info.push_inst($program, br);
+        // generate right-hand side expression
+        info.push_bb($program, $rhs_bb);
+        let rhs = $rhs
+            .generate($program, $scopes)?
+            .into_int($program, $scopes)?;
+        let info = cur_func_mut!($scopes);
+        let rhs = info.new_value($program).binary(BinaryOp::NotEq, rhs, zero);
+        info.push_inst($program, rhs);
+        let store = info.new_value($program).store(rhs, result);
+        info.push_inst($program, store);
+        // generate jump
+        let jump = info.new_value($program).jump($end_bb);
+        info.push_inst($program, jump);
+        info.push_bb($program, $end_bb);
+        // generate load
+        let load = info.new_value($program).load(result);
+        info.push_inst($program, load);
+        Ok(ExpValue::Int(load))
+    }};
+}
+
+impl<'ast> GenerateProgram<'ast> for LOrExp {
+    type Out = ExpValue;
+
+    fn generate(&'ast self, program: &mut Program, scopes: &mut Scopes<'ast>) -> Result<Self::Out> {
+        match self {
+            Self::LAnd(exp) => exp.generate(program, scopes),
+            Self::LOrLAnd(lhs, rhs) => generate_logical_ops! {
+              lhs, rhs, program, scopes, "lor", rhs_bb, end_bb, end_bb, rhs_bb
+            },
+        }
+    }
+}
+
+impl<'ast> GenerateProgram<'ast> for LAndExp {
+    type Out = ExpValue;
+
+    fn generate(&'ast self, program: &mut Program, scopes: &mut Scopes<'ast>) -> Result<Self::Out> {
+        match self {
+            Self::Eq(exp) => exp.generate(program, scopes),
+            Self::LAndEq(lhs, rhs) => generate_logical_ops! {
+              lhs, rhs, program, scopes, "land", rhs_bb, end_bb, rhs_bb, end_bb
+            },
+        }
+    }
+}
+
+impl<'ast> GenerateProgram<'ast> for EqOp {
+    type Out = BinaryOp;
+
+    fn generate(&'ast self, _: &mut Program, _: &mut Scopes<'ast>) -> Result<Self::Out> {
+        Ok(match self {
+            EqOp::Eq => BinaryOp::Eq,
+            EqOp::Neq => BinaryOp::NotEq,
+        })
+    }
+}
+
+impl<'ast> GenerateProgram<'ast> for EqExp {
+    type Out = ExpValue;
+
+    fn generate(&'ast self, program: &mut Program, scopes: &mut Scopes<'ast>) -> Result<Self::Out> {
+        match self {
+            Self::Rel(exp) => exp.generate(program, scopes),
+            Self::EqRel(lhs, op, rhs) => {
+                let lhs = lhs.generate(program, scopes)?.into_int(program, scopes)?;
+                let rhs = rhs.generate(program, scopes)?.into_int(program, scopes)?;
+                let op = op.generate(program, scopes)?;
+                let info = cur_func!(scopes);
+                let value = info.new_value(program).binary(op, lhs, rhs);
+                info.push_inst(program, value);
+                Ok(ExpValue::Int(value))
+            }
+        }
+    }
+}
+
+impl<'ast> GenerateProgram<'ast> for RelExp {
+    type Out = ExpValue;
+
+    fn generate(&'ast self, program: &mut Program, scopes: &mut Scopes<'ast>) -> Result<Self::Out> {
+        match self {
+            Self::Add(exp) => exp.generate(program, scopes),
+            Self::RelAdd(lhs, op, rhs) => {
+                let lhs = lhs.generate(program, scopes)?.into_int(program, scopes)?;
+                let rhs = rhs.generate(program, scopes)?.into_int(program, scopes)?;
+                let op = op.generate(program, scopes)?;
+                let info = cur_func!(scopes);
+                let value = info.new_value(program).binary(op, lhs, rhs);
+                info.push_inst(program, value);
+                Ok(ExpValue::Int(value))
+            }
+        }
+    }
+}
+
+impl<'ast> GenerateProgram<'ast> for AddExp {
+    type Out = ExpValue;
+
+    fn generate(&'ast self, program: &mut Program, scopes: &mut Scopes<'ast>) -> Result<Self::Out> {
+        match self {
+            Self::Mul(exp) => exp.generate(program, scopes),
+            Self::AddMul(lhs, op, rhs) => {
+                let lhs = lhs.generate(program, scopes)?.into_int(program, scopes)?;
+                let rhs = rhs.generate(program, scopes)?.into_int(program, scopes)?;
+                let op = op.generate(program, scopes)?;
+                let info = cur_func!(scopes);
+                let value = info.new_value(program).binary(op, lhs, rhs);
+                info.push_inst(program, value);
+                Ok(ExpValue::Int(value))
+            }
+        }
+    }
+}
+
+impl<'ast> GenerateProgram<'ast> for AddOp {
+    type Out = BinaryOp;
+
+    fn generate(&'ast self, _: &mut Program, _: &mut Scopes<'ast>) -> Result<Self::Out> {
+        Ok(match self {
+            AddOp::Add => BinaryOp::Add,
+            AddOp::Sub => BinaryOp::Sub,
+        })
+    }
+}
+
+impl<'ast> GenerateProgram<'ast> for RelOp {
+    type Out = BinaryOp;
+
+    fn generate(&'ast self, _: &mut Program, _: &mut Scopes<'ast>) -> Result<Self::Out> {
+        Ok(match self {
+            RelOp::Lt => BinaryOp::Lt,
+            RelOp::Gt => BinaryOp::Gt,
+            RelOp::Le => BinaryOp::Le,
+            RelOp::Ge => BinaryOp::Ge,
+        })
+    }
+}
+
+impl<'ast> GenerateProgram<'ast> for MulOp {
+    type Out = BinaryOp;
+
+    fn generate(&'ast self, _: &mut Program, _: &mut Scopes<'ast>) -> Result<Self::Out> {
+        Ok(match self {
+            MulOp::Mul => BinaryOp::Mul,
+            MulOp::Div => BinaryOp::Div,
+            MulOp::Mod => BinaryOp::Mod,
+        })
+    }
+}
+
+impl<'ast> GenerateProgram<'ast> for MulExp {
+    type Out = ExpValue;
+
+    fn generate(&'ast self, program: &mut Program, scopes: &mut Scopes<'ast>) -> Result<Self::Out> {
+        match self {
+            Self::Unary(exp) => exp.generate(program, scopes),
+            Self::MulUnary(lhs, op, rhs) => {
+                let lhs = lhs.generate(program, scopes)?.into_int(program, scopes)?;
+                let rhs = rhs.generate(program, scopes)?.into_int(program, scopes)?;
+                let op = op.generate(program, scopes)?;
+                let info = cur_func!(scopes);
+                let value = info.new_value(program).binary(op, lhs, rhs);
+                info.push_inst(program, value);
+                Ok(ExpValue::Int(value))
+            }
+        }
+    }
+}
